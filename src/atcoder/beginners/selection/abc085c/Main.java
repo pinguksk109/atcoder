b8c86293b1a0b13e8fd56931757c14dc4463152e
Keisuke Ota <k.ota109@outlook.com>
2023-06-21 01:51:08 +0900

    お年玉問題　実装中

--- src/atcoder/beginners/selection/abc085c/Main.java
+++ src/atcoder/beginners/selection/abc085c/Main.java
@@ -12,4 +12,21 @@ public class Main {
 		
 		getTotalAmount(moneyCount, sumMoney);
 	}
+
+	private static void getTotalAmount(Integer moneyCount, Integer sumMoney) {
+		
+		for(int x = 0; x <= moneyCount; x++) {
+			for(int y = 0; y <= moneyCount - x; y++) {
+				int remainingMoney = sumMoney - (10000 * x + 5000 * y);
+				int yRemaining = moneyCount - x - y;
+				
+				if(remainingMoney == 1000 * yRemaining) {
+					System.out.println(x + " " + y + " " + yRemaining);
+					break;
+				} else {
+					System.out.println("-1 -1 -1");
+				}
+			}
+		}
+	}
 }
